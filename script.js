7844d2e14258cbfc8d260c0d9758f4d064db8fb4
tony8639 <106917549+tony8639@users.noreply.github.com>
2022-09-07 16:14:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var links=document.getElementById("navlink")
+
+function hideMenu(){
+    // console.log("inhidemenu");
+    links.style.right="-250px";
+}
+
+function showMenu(){
+    // console.log("inshowmenu");
+    links.style.right="0";
+}
+
+
